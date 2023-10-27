62537a5fc81622b8ea799e7d1b4e697c27c7dd83
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-10-27 19:45:35 +0300

    Complete `example` assignment

---
+++ example/assignment.sbt
@@ -0,0 +1,13 @@
+// Student tasks (i.e. submit, packageSubmission)
+enablePlugins(StudentTasks)
+
+assignmentVersion.withRank(KeyRanks.Invisible) := "72f17037"
+
+
+courseraId := ch.epfl.lamp.CourseraId(
+  courseId = "authoringBranch~jQr2Ee0lEeq0Lg4u7uf-_Q",
+  key = "W-pW4ArNSQ-7M2tJn-dLWQ",
+  itemId = "EoeMo",
+  premiumItemId = None,
+  partId = "vLfGt"
+)
---
+++ example/src/main/scala/example/Lists.scala
@@ -0,0 +1,57 @@
+package example
+
+import scala.annotation.tailrec
+import java.{util => ju}
+
+object Lists:
+
+  /**
+   * This method computes the sum of all elements in the list xs. There are
+   * multiple techniques that can be used for implementing this method, and
+   * you will learn during the class.
+   *
+   * For this example assignment you can use the following methods in class
+   * `List`:
+   *
+   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+   *    is empty an exception is thrown
+   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+   *    list `xs` without its `head` element
+   *
+   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   *  solution.
+   *
+   * @param xs A list of natural numbers
+   * @return The sum of all elements in `xs`
+   */
+  @tailrec
+  def sum(xs: List[Int], acc: Int = 0): Int = xs match {
+      case Nil => acc
+      case x :: xs => sum(xs, acc + x)
+    }
+
+  /**
+   * This method returns the largest element in a list of integers. If the
+   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+   *
+   * You can use the same methods of the class `List` as mentioned above.
+   *
+   * ''Hint:'' Again, think of a recursive solution instead of using looping
+   * constructs. You might need to define an auxiliary method.
+   *
+   * @param xs A list of natural numbers
+   * @return The largest element in `xs`
+   * @throws java.util.NoSuchElementException if `xs` is an empty list
+   */
+  def max(xs: List[Int]): Int =
+    if xs.isEmpty then throw ju.NoSuchElementException("empty list")
+
+    @tailrec
+    def max(xs: List[Int], minVal: Int): Int = xs match {
+      case Nil => minVal
+      case x :: xs => max(xs, if x > minVal then x else minVal)
+    }
+
+    max(xs.tail, xs.head)
+  end max

482e23eef23abcf3597cc35c27c26413dbe6bb8f
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2023-11-06 09:41:39 +0200

    Finish RecFun excercises

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object RecFun extends RecFunInterface:
 
   def main(args: Array[String]): Unit =
@@ -12,14 +14,42 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    @tailrec
+    def findTargetVal(currRow: List[Int], prevRow: List[Int]): Int =
+      val currRowIdx = prevRow.length
+      val colToCalc = currRow.length
+      def nextRowVal = if colToCalc == 0 || colToCalc == currRowIdx then 1 else
+        prevRow(colToCalc - 1) + prevRow(colToCalc)
+
+      if colToCalc == c && currRowIdx == r then nextRowVal
+      else if currRow.length > prevRow.length then findTargetVal(List(), currRow)
+      else findTargetVal(currRow :+ nextRowVal, prevRow)
+
+    findTargetVal(List(), List())
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean =
+    @tailrec
+    def balance(chars: List[Char], count: Int): Boolean = chars match
+      case _ if count < 0 => false
+      case Nil => count == 0
+      case head :: tail if head == '(' => balance(tail, count + 1)
+      case head :: tail if head == ')' => balance(tail, count - 1)
+      case _ => balance(chars.tail, count)
+
+    balance(chars, 0)
+
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    def countChangeInner(money: Int, coins: List[Int]): Int =
+      if (money < 0 || coins.isEmpty) then 0
+      else if (money == 0) then 1
+      else countChangeInner(money, coins.tail) + countChangeInner(money - coins.head, coins)
+
+    countChangeInner(money, coins)
